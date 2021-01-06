d84f1d9353b36b7906240a075be7cf57563dff5d
barbara0914 <76997727+barbara0914@users.noreply.github.com>
2021-01-06 16:28:28 +0100

    Update lokalisation_test.py

--- lokalisation_test.py
+++ lokalisation_test.py
@@ -1,18 +1,19 @@
-from freefield import camera, setup
 import slab
-import os
-import sys
-sys.path.append("C:/Projects/freefield_toolbox")
-sys.path.append("C:/Projects/soundtools")
-_location = setup._location
+import time
+from freefield import main
+import numpy as np
+from pathlib import Path
 
+proc_list = [["RX81", "RX8", "path/play_buffer_from_channel.rcx"],
+             ["RX82", "RX8", "path/play_buffer_from_channel.rcx"],
+             ["RP2", "RP2", "path/button_response.rcx"]]
 
 # Initialize
-setup.set_speaker_config("dome")
+setup.set_speaker_config("arc")
 camera.init(type="freefield")
 
 # Define relevant variables for setup.localization_test()
-speakers = [(0, 37.5), (0, 12.5), (0, -12.5), (0, -37.5)]
+SPEAKERS = main.get_speaker_list(list(range(9, 25)))  # 13 speakers, 24 in the middle, 6 to each side
 sound = slab.Sound.whitenoise(duration=1.0, samplerate=48828)
 
 
