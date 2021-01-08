fcb68c0f253c5ea90046186b81fb81ab34d9d40a
barbara0914 <76997727+barbara0914@users.noreply.github.com>
2021-01-08 13:30:38 +0100

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -3,7 +3,8 @@ import time
 from freefield import main
 import numpy as np
 from pathlib import Path
-DIR = Path(__file__).parent.resolve()  # location of the main folder
+DIR = Path(__file__).parent.resolve() 
+# location of the main folder
 
 proc_list = [["RX81", "RX8", "path/play_buffer_from_channel.rcx"],
              ["RX82", "RX8", "path/play_buffer_from_channel.rcx"],
@@ -21,7 +22,7 @@ def priming(kind='positive', n_files=7):  # 'negative', or 'neutral
     if kind not in ('positive', 'negative', 'neutral'):
         raise ValueError('Unknown run type. Should be positive, negative, or neutral.')
     input(f'Show {kind} priming to participant!')
-    # randomly choose one of the 7 files to play
+# randomly choose one of the 7 files to play
     file = DIR / "stimuli" / f"{np.random.randint(n_files}.wav"
     
     priming_stimulus = slab.Sound.read(str(file)).channel(0)
@@ -77,7 +78,7 @@ def block(SPEAKERS, FILES, kind='positive', stimlevel=90, noiselevel=80):  # 'ne
             while not main.get_variable("response", proc="RP2"):
                 time.sleep(0.01)  # wait for response to stimulus
             ele, azi = main.get_headpose()
-            response.append([y])
+            main.append([y])
 
    
 """
@@ -86,8 +87,8 @@ def run_experiment(subject, repeat_files=2, repeat_conditions=2):
 
     out_folder = EXPDIR/Path(subject)
     # experiment
-    setup.set_speaker_config("arc")
-    setup.initialize_devices(RX81_file=rx81_path, RP2_file=rp2_path, ZBus=True, cam=True)
+    main.set_speaker_config("arc")
+    main.initialize_devices(RX81_file=rx81_path, RP2_file=rp2_path, ZBus=True, cam=True)
     conditions = slab.Trialsequence(conditions=["positive", "negative", "neutral"],
                                     n_reps=repeat_conditions, kind='non_repeating')
     # conditions.insert(0, "noise") ??
